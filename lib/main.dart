cb4d97512dfc6eb8960318f6ef5190949cdccd9d
yeimy <yeimyg833@gmail.com>
2025-07-28 12:54:37 -0400

    Avance login

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,52 @@ void main(){//metodo
 class MyApp extends StatelessWidget {//clase
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       title: 'Formulario de registro',//texto plano
-home: Scaffold(
-  appBar: AppBar(
-    title: Text('Inicia Sesion en SoftKids'),
-  ),
-),//estructura del cuerpo
-    );
+         theme: ThemeData(
+             primarySwatch: Colors.deepPurple,
+            primaryColor: const Color(0xFF1E3A8A),
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color(0xFF1E3A8A),
+              secondary: const Color(0xFF6B7280),
+            ),
+              useMaterial3: true),
+      home: const LoginFormScreen(),
+      debugShowCheckedModeBanner: false,
+      );
     throw UnimplementedError();
+  }
+}
+
+class LoginFormScreen extends StatelessWidget{
+  const LoginFormScreen({super.key});
+
+  static const Color primaryNavy = Color(0xFF1E3A8A);
+  static const Color backgroundGrey = Color(0xFFF9FAFB);
+  static const Color secondaryGrey = Color(0xFF6B7280);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: backgroundGrey,
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(24),
+          width: 400,
+          decoration: BoxDecoration(
+            color: Colors.deepOrange,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: secondaryGrey.withOpacity(0.1),
+                blurRadius: 10,
+                offset: const Offset(0, 4),
+              )
+            ]
+          ),
+        ),
+      ),
+    );
   }
 }
